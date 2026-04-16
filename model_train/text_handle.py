4fb40f8d747fb3741a487e57afd87d0cfed1c679
recover-DEV <pqy2004@foxmail.com>
2026-04-16 21:14:43 +0800

    prework

---
+++ model_train/text_handle.py
@@ -0,0 +1,89 @@
+import hanlp
+import re
+
+_tokenizer = None
+
+def _get_tokenizer():
+    global _tokenizer
+    if _tokenizer is None:
+        _tokenizer = hanlp.load(hanlp.pretrained.tok.COARSE_ELECTRA_SMALL_ZH)
+    return _tokenizer
+
+def _filter_tokens(tokens):
+    return [t for t in tokens if re.search(r'[\u4e00-\u9fff\w]', t)]
+
+def intro_tokenize_text(text):
+    tokens = _get_tokenizer()(text)
+    return _filter_tokens(tokens)
+
+def intro_tokenize_batch(text_list, batch_size=64):
+    tokenizer = _get_tokenizer()
+    result = []
+    for i in range(0, len(text_list), batch_size):
+        batch = text_list[i:i + batch_size]
+        batch_tokens = tokenizer(batch)
+        result.extend([_filter_tokens(tokens) for tokens in batch_tokens])
+    return result
+
+def classify_tokenize_text(text):
+    return list(text)
+
+def tokenize_text(text_list, tokenizer):
+    result = list()
+    for text in text_list:
+        result.append(tokenizer(text))
+    return result
+
+class WordCount:
+    def __init__(self, text_list, min_freq=0):
+        self.text_list = text_list
+        self.min_freq = min_freq
+        self.word_count = dict()
+        self.word_index = dict()
+        self.count_word()
+        self.build_word_index()
+    
+    def count_word(self):
+        for text in self.text_list:
+            for token in text:
+                if token not in self.word_count:
+                    self.word_count[token] = 1
+                else:
+                    self.word_count[token] += 1
+        return self.word_count
+    
+
+    def build_word_index(self):
+        self.index_word = dict()
+        for word, count in self.word_count.items():
+            if count >= self.min_freq:
+                idx = len(self.word_index)
+                self.word_index[word] = idx
+                self.index_word[idx] = word
+        return self.word_index
+
+    def __getitem__(self, query):
+        if isinstance(query, int):
+            return self.index_word[query]
+        return self.word_index[query]
+
+    def __len__(self):
+        return len(self.word_index)
+
+
+if __name__ =="__main__":
+    print("hello world")
+    text_list=["本书共1140个词条,分五个部分:第一部分1014条,包括有价证券及证券市场、股票、债券、票据、凭单",
+    "本书才TI公司的DSP芯片为例，介绍了DSP的原理和应用方法，首先介绍了DSP的发展过程，引入TI公",
+    "本书从交互设计的角度出发，讲述了用户研究的基础知识以及问卷、访谈、用户画像、数据分析等最常用的研究方",
+    "The book is about about the user research and the user experience design, and the user research is a very important part of the user experience design."]
+    
+    token_list = tokenize_text(text_list, intro_tokenize_text)
+    print(token_list)
+
+    intro_word = WordCount(token_list)
+    for i in range(10):
+        print(intro_word[i])
+        print(intro_word[intro_word[i]])
+
+
--- model_train/train.py
+++ model_train/train.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+from read_data import build_dataset, TextDataset
+
+file_path = "data_set/book2019-2023.csv"
+
+intro_token_list, classify_token_list = build_dataset(file_path)
+print(intro_token_list[0])
+print(classify_token_list[0])
+dataset = TextDataset(intro_token_list, classify_token_list, min_freq=1)
+
+print(dataset[0])
