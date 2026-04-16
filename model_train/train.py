eb1c71dc43d948a2f5909684297a326befcecdcc
recover-DEV <pqy2004@foxmail.com>
2026-04-16 21:27:23 +0800

    Refactor project structure

--- model_train/train.py
+++ model_train/train.py
@@ -1,6 +1,9 @@
-from read_data import build_dataset, TextDataset
+import os
+from utils.read_data import build_dataset, TextDataset
 
-file_path = "data_set/book2019-2023.csv"
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(BASE_DIR, "data_set/book2019-2023.csv")
 
 intro_token_list, classify_token_list = build_dataset(file_path)
 print(intro_token_list[0])
--- model_train/utils/read_data.py
+++ model_train/utils/read_data.py
@@ -1,26 +1,14 @@
 from torch.utils.data import Dataset
-from text_handle import WordCount, tokenize_text, intro_tokenize_text, intro_tokenize_batch
+from .text_handle import WordCount, intro_tokenize_batch
 import pandas as pd
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def read_data(file_path):
-    data = pd.read_csv(os.path.join(BASE_DIR, file_path))
+    data = pd.read_csv(file_path)
     data = data[['introduction', 'refer_class_no']].dropna()
     return data
 
 def build_dataset(file_path):
-    data = pd.read_csv(os.path.join(BASE_DIR, file_path))
-    data = data.dropna(subset=['introduction', 'refer_class_no'])
-    intro_list = data['introduction'].tolist()
-    classify_list = data['refer_class_no'].tolist()
-    intro_token_list = intro_tokenize_batch(intro_list)
-    classify_token_list = [list(cls) for cls in classify_list]
-    return intro_token_list, classify_token_list
-
-def build_dataset(file_path):
-    data = pd.read_csv(os.path.join(BASE_DIR, file_path))
+    data = pd.read_csv(file_path)
     data = data.dropna(subset=['introduction', 'refer_class_no'])
     intro_list = data['introduction'].tolist()
     classify_list = data['refer_class_no'].tolist()
@@ -32,16 +20,16 @@ class TextDataset(Dataset):
     def __init__(self, intro_token_list, classify_token_list, min_freq=1):
         self.vocab = WordCount(intro_token_list, min_freq=min_freq)
         self.label_vocab = WordCount(classify_token_list, min_freq=min_freq)
-        self.X = [[self.vocab[t] for t in tokens if t in self.vocab.word_index]
+        self.features = [[self.vocab[t] for t in tokens if t in self.vocab.word_index]
                   for tokens in intro_token_list]
-        self.y = [[self.label_vocab[t] for t in tokens if t in self.label_vocab.word_index]
+        self.labels = [[self.label_vocab[t] for t in tokens if t in self.label_vocab.word_index]
                   for tokens in classify_token_list]
 
     def __len__(self):
-        return len(self.X)
+        return len(self.features)
 
     def __getitem__(self, idx):
-        return self.X[idx], self.y[idx]
+        return self.features[idx], self.labels[idx]
 
 if __name__ == "__main__":
     intro_token_list, classify_token_list = build_dataset("data_set/book2019-2023.csv")
